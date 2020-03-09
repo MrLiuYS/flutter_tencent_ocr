03e6d952bcf614d01c88f4d6b7c80521e1f80921
MrLiuYS <3050700400@qq.com>
2020-03-09 16:38:02 +0800

    clean

--- ios/Classes/FlutterTencentOcrPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterTencentOcrPlugin.h"
-#import <flutter_tencent_ocr/flutter_tencent_ocr-Swift.h>
-
-@implementation FlutterTencentOcrPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterTencentOcrPlugin registerWithRegistrar:registrar];
-}
-@end
